f96734bfcce60618e3a5f04144e5ec4a9dded68a
Joseph <josephhhenrique318@gmail.com>
2026-02-05 13:07:38 -0300

    maps

--- bus/src/components/Maps/index.jsx
+++ bus/src/components/Maps/index.jsx
@@ -3,10 +3,10 @@ import './index.css';
 
 
 function Maps() {
-  
+
   return (
     <section className="container-fluid map" style={{ height: 'auto' }}>
-       <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3988.644812334774!2d-48.393043525034216!3d-1.3896797985971905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMcKwMjMnMjIuOSJTIDQ4wrAyMycyNS43Ilc!5e0!3m2!1spt-BR!2sbr!4v1770306105647!5m2!1spt-BR!2sbr"
+       <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3988.644812334774!2d-48.393043525034216!3d-1.3896797985971905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMcKwMjMnMjIuOSJTIDQ4wrAyMycyNS43Ilc!5e0!3m2!1spt-BR!2sbr!4v1770307567584!5m2!1spt-BR!2sbr"
         width="100%"
         height="450"
         style={{ border: 0 }} // Aqui você corrige o estilo para ser um objeto
